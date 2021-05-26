c6a4b19b2e42b4c660cf563f98cc15f2ca19b42e
Daniel <dat64558@gmail.com>
2021-05-26 09:34:36 -0500

    Moviendo Controladores rest

--- agroiot/agroiot/src/main/java/com/co/proyecto/agroiot/controlador/HumedadControlador.java
+++
@@ -1,5 +0,0 @@
-package com.co.proyecto.agroiot.controlador;
-
-public class HumedadControlador {
-
-}
--- agroiot/agroiot/src/main/java/com/co/proyecto/agroiot/controlador/TemperaturaControlador.java
+++
@@ -1,7 +0,0 @@
-package com.co.proyecto.agroiot.controlador;
-
-import org.springframework.stereotype.Controller;
-
-public class TemperaturaControlador {
-
-}
