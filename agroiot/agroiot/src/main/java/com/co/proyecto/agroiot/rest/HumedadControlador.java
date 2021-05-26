7b224b3f69b434af8fd8dfa1d162db0f90d5b453
Daniel <dat64558@gmail.com>
2021-05-26 09:53:02 -0500

    Moviendo controlador rest Humedad

---
+++ agroiot/agroiot/src/main/java/com/co/proyecto/agroiot/rest/HumedadControlador.java
@@ -0,0 +1,10 @@
+package com.co.proyecto.agroiot.rest;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/humedad")
+public class HumedadControlador {
+
+}
